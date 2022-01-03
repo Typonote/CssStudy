5e02f3d404b72ba7215a2d3b03c6da34cd465999
JinKyung Kim <kimradium@naver.com>
2022-01-03 21:39:48 +0900

    #2 Life Before Grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Father">
       <div className="box">1</div>
       <div className="box">2</div>
       <div className="box">3</div>
